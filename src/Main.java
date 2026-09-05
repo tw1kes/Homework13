0c43c4dcfd92c725db1cd11ac411f6d6d70600dc
User <dutova_evgen@mail.ru>
2026-09-05 11:07:07 +0500

    проверьте пожалуйста ДЗ после исправлений 2

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,17 @@ public class Main {
     }
 
     public static void installApp(int clientDeviceYear, int clientOS) {
-        int clientOperSystem = 1;
-        if (clientOperSystem == 0 && clientDeviceYear < 2015) {
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOperSystem == 0 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOperSystem == 1 && clientDeviceYear < 2015) {
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOperSystem == 1 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Ошибка");
         }
-    }
-    {
-        int clientOS = 0;
-        int clientDeviceYear = 2015;
     }
 
     public static int printDeliveryTime(int deliveryDistance) {
